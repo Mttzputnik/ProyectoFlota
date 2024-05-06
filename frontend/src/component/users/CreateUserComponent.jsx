b1d693faccf31b89732acfb3d45cbfdba7515473
Jonvas06 <jon.vasquez23414@ucaldas.ed.co>
2024-05-06 07:59:10 -0500

    feacture create user on react

--- frontend/src/component/users/CreateUserComponent.jsx
+++ frontend/src/component/users/CreateUserComponent.jsx
@@ -16,7 +16,7 @@ export const CreateUserComponent = () => {
     current_password:"" //debe coincidir con prisma validar
   })
     
-const handlechangen =(event)=>{
+const handlechange =(event)=>{
   setFormData({...FormData,[event.target.id]:event.target.value});
 };
 
@@ -25,9 +25,58 @@ const handleAvatarChange =(event) => {
 }
 
 
+const handleCreateUser = async (e)=> {
+  e.preventDefault();
+  try {
+    const formDataToSend = new FormData();
+    formDataToSend.append("email",FormData.email);
+    formDataToSend.append("user_name",FormData.user_name);
+    formDataToSend.append("last_name",FormData.last_name);
+    formDataToSend.append("avatar", FormData.avatar);
+    formDataToSend.append("current_password",FormData.current_password);
+
+    console.log(formDataToSend);
+
+
+  } catch (error) {
+    
+  }
+
+
+
+}
+
 
 
   return (
-    <div>CreateUserComponent</div>
+    <>
+    <h2>Create User</h2>
+    <form onSubmit={handleCreateUser} encType='multipart/form-data'>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input type="email" id='email' value={FormData.email} onChange={handlechange} defaultValue={'test@test.co'} required/>
+        
+      </div>
+      <div>
+        <label htmlFor='user_name'>Nombre(s)</label>
+          <input type="text" id='user_name' value={FormData.user_name} onChange={handlechange} defaultValue={'john'} required/>
+      </div>
+      <div>
+        <label htmlFor='last_name'>Apellido(s)</label>
+          <input type="text" id='last_name' value={FormData.last_name} onChange={handlechange} defaultValue={'doe'} required/>
+      </div>
+      <div>
+        <label htmlFor='avatar'>avatar</label>
+          <input type="file" id='avatar' value={FormData.avatar} onChange={handleAvatarChange} accept='image/*'/>
+      </div>
+      <div>
+        <label htmlFor='current_password'>Password</label>
+          <input type="password" id='current_password' value={FormData.current_password} onChange={handleAvatarChange} required/>
+      </div>
+
+      <button type='submit'>Create User</button>
+    </form>
+    
+    </>
   )
 }
