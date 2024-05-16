b26d8ab9b5c0b24d451f6ddf50615ffefc462617
stiven <johan.1701922249@ucaldas.edu.co>
2024-05-16 16:52:57 -0500

    fix errors

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,106 +1,83 @@
-import { PATHS } from "../utils/config"
+import { PATHS } from "../utils/config";
 
 export class User {
-    baseApi = PATHS.BASE_PATH;
-    newUsersPath = PATHS.API_ROUTES.NEW_USER;
-    usersPath = PATHS.API_ROUTES.USERS;
+  BASE_PATH = PATHS.BASE_PATH;
+  USERS = PATHS.API_ROUTES.USERS;
+  NEW_USERS = PATHS.API_ROUTES.NEW_USER;
+  EDIT_USER = PATHS.API_ROUTES.EDIT_USER;
 
-    createUser = async (formData) => {
-        
-        try {
-            console.log("api", formData);
-            console.log(`${this.baseApi} ${PATHS.USER}`);
-            const URL = `${this.baseApi}${this.newUsersPath}`
-            const params =
-                {
-                    method: "POST",
-                    body: formData
-                };
-            console.log(params);
-            const response = await fetch(URL, params)
-            console.log(response);
-            const result = response.json();
-            console.log(result);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    }
-    getUsers = async () => {
-        try {
-        } catch (error) {
-
-        }
-    }
-    getUserById = async () => {
-        try {
-            const URL = `${this.BASE_PATH}/${this.UserPath}`;
-            console.log(URL);
-            const response = await fetch(URL);
-            const users = response.json();
-            return users;
-          } catch (error) {
-            console.error(error);
-          }
-        };
-
-    getUsersById = async(userId) => {
-        try {
-            const URL = `${this.BASE_PATH}/${this.USERS}/${userId}`;
-            console.log(URL);
-            const response = await fetch(URL);
-            const user = response.json();
-            return user;
-    } catch (error) {
-        console.error(error);
-    }};
-
-    editUsersById = async(userId, FormData) => {
-        try {
-            const URL = `${this.BASE_PATH}/${this.USERS}/${userId}`;
-            console.log(URL);
-            const params = {
-                method: 'PUT',
-                body: JSON.stringify(FormData),
-            };
-            const response = await fetch(URL, params);
-            const result =await response.json();
-            console.log(result);
-            return result;
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    deleteUsersById = async(userId) => {
-        try {
-            const URL = `${this.BASE_PATH}/${this.USERS}/${userId}`;
-            console.log(URL);
-            const params = {
-                method: 'DELETE',
-            };
-            const response = await fetch(URL, params);
-            const message =await response.text();
-            console.log(message);
-            return message;
-        } catch (error) {
-            console.error(error);
-        }
-    };
-getUserByIdAndUpdate = async () => {
+  createUser = async (formData) => {
     try {
-
+      console.log("api", formData);
+      const URL = `${this.BASE_PATH}/${this.NEW_USERS}`;
+      console.log(URL);
+      const params = {
+        method: "POST",
+        body: formData,
+      };
+      const response = await fetch(URL, params);
+      const result = await response.json();
+      console.log(result);
     } catch (error) {
-
+      console.error(error);
+      throw error; 
     }
-}
-
-
-getUserByIdAndDelete = async () => {
+  };
+  
+  getUsers = async () => {
     try {
-
+      const URL = `${this.BASE_PATH}/${this.USERS}`;
+      console.log(URL);
+      const response = await fetch(URL);
+      const users = await response.json();
+      return users; 
     } catch (error) {
-
+      console.error(error);
     }
+  };
+
+  getUserById = async (userId) => {
+    try {
+      const URL = `${this.BASE_PATH}/${this.USERS}/${userId}`;
+      console.log(URL);
+      const response = await fetch(URL);
+      const user = await response.json();
+      return user; 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  editUserById = async (userId, updatedData) => {
+    try {
+      const URL = `${this.BASE_PATH}/${this.EDIT_USER}/${userId}`;
+      console.log(URL);
+      const params = {
+        method: "PATCH",
+        body: JSON.stringify(updatedData),
+      };
+      const response = await fetch(URL, params);
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  deleteUserById = async (userId) => {
+    try {
+      const URL = `${this.BASE_PATH}/${this.USERS}/delete/${userId}`;
+      console.log(URL);
+      const params = {
+        method: "DELETE",
+      };
+      const response = await fetch(URL, params);
+      const message = await response.text(); // Convert response to text
+      console.log(message);
+      return message; 
+    } catch (error) {
+      console.error(error);
+    }
+  };
 }
--- frontend/src/component/users/ListComponent.jsx
+++ frontend/src/component/users/ListComponent.jsx
@@ -1,31 +1,210 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { User } from "../../api/user";
+import { getUsers, editUserById, deleteUserById } from "../../slices/userSlice";
+import {
+  Table,
+  Avatar,
+  Space,
+  Tooltip,
+  Modal,
+  Form,
+  Input,
+  Switch,
+  Upload,
+  Button,
+} from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { UploadOutlined } from "@ant-design/icons";
+
+const { confirm } = Modal;
 
 export const ListComponent = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.user.users);
   const userApi = new User();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-  useEffect(async () => {
-    await userApi.getUsers();
-  }, []);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await userApi.getUsers();
+        dispatch(getUsers(usersData));
+      } catch (error) {
+        console.error("Failed to fetch users", error);
+      }
+    };
+
+    fetchUsers();
+  }, [dispatch]);
+
+  const handleEdit = (id) => {
+    const user = users.find((user) => user.id === id);
+    setSelectedUser(user);
+    setIsModalVisible(true);
+  };
+
+  const handleDelete = (id) => {
+    confirm({
+      title: "Seguro quieres eliminar este usuario?",
+      content: "Esta opción no se puede revertir",
+      onOk() {
+        userApi
+          .deleteUserById(id)
+          .then(() => {
+            dispatch(deleteUserById(id));
+          })
+          .catch((error) => {
+            console.error("Failed to delete user", error);
+          });
+      },
+      onCancel() {
+        console.log("Cancel delete");
+      },
+    });
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setSelectedUser(null);
+  };
+
+  const handleOk = () => {
+    userApi
+      .editUserById(selectedUser.id, selectedUser)
+      .then((result) => {
+        dispatch(
+          editUserById({ userId: selectedUser.id, updatedUserData: result })
+        );
+        setIsModalVisible(false);
+        setSelectedUser(null);
+      })
+      .catch((error) => {
+        console.error("Failed to edit user", error);
+      });
+  };
+
+  const handleChange = (e) => {
+    setSelectedUser({
+      ...selectedUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSwitchChange = (checked) => {
+    setSelectedUser({
+      ...selectedUser,
+      active_user: checked,
+    });
+  };
+
+  const columns = [
+    {
+      title: "Avatar",
+      dataIndex: "avatar",
+      key: "avatar",
+      render: (text, record) => <Avatar src={record.avatar} />,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "User Name",
+      dataIndex: "user_name",
+      key: "user_name",
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      title: "Active",
+      dataIndex: "active_user",
+      key: "active_user",
+      render: (text, record) => (
+        <span>{record.active_user ? "Yes" : "No"}</span>
+      ),
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text, record) => (
+        <Space size="middle">
+          <Tooltip title="Edit">
+            <EditOutlined
+              style={{ color: "blue", cursor: "pointer" }}
+              onClick={() => handleEdit(record.id)}
+            />
+          </Tooltip>
+          <Tooltip title="Delete">
+            <DeleteOutlined
+              style={{ color: "red", cursor: "pointer" }}
+              onClick={() => handleDelete(record.id)}
+            />
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
 
   return (
-    <>
-    <h2>Users List</h2>
-    {users.length > 0 ? (
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <span>{user.email}</span>
-            <span>{user.user_name} {user.last_name}</span>
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <p>No users found.</p>
-    )}
-    </>
+    <div className="container">
+      <h2>Users List</h2>
+      <Table dataSource={users} columns={columns} rowKey="id" />
+      {selectedUser && (
+        <Modal
+          title="Edit User"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}>
+          <Form>
+            <Form.Item label="Email">
+              <Input
+                name="email"
+                value={selectedUser.email}
+                onChange={handleChange}
+              />
+            </Form.Item>
+            <Form.Item label="User Name">
+              <Input
+                name="user_name"
+                value={selectedUser.user_name}
+                onChange={handleChange}
+              />
+            </Form.Item>
+            <Form.Item label="Last Name">
+              <Input
+                name="last_name"
+                value={selectedUser.last_name}
+                onChange={handleChange}
+              />
+            </Form.Item>
+            <Form.Item label="Active">
+              <Switch
+                checked={selectedUser.active_user}
+                onChange={handleSwitchChange}
+              />
+            </Form.Item>
+            <Form.Item label="Avatar">
+              <Upload
+                accept="image/*"
+                beforeUpload={(file) => {
+                  return false;
+                }}
+                onChange={(info) => {
+                  console.log(info.fileList);
+                }}
+                fileList={[]}>
+                <Button icon={<UploadOutlined />}>Seleccionar archivo</Button>
+              </Upload>
+            </Form.Item>
+          </Form>
+        </Modal>
+      )}
+    </div>
   );
 };
