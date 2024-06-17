af1e7ada6b9f81d123b09e897966bcac6fbd9e12
Jonvas06 <jon.vasquez23414@ucaldas.ed.co>
2024-06-17 00:24:37 -0500

    feature: se implementa la logica para el calculo de las rutas en el backend

--- backend/src/routes/optimizedRoute_routes.js
+++ backend/src/routes/optimizedRoute_routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // Rutas para las operaciones del CRUD de OptimizedRoute
 router.post("/new-optimizedRoute", optimizedRouteController.createOptimizedRoute);
+router.post('/calculate', optimizedRouteController.calculateRoute);
 router.get("/", optimizedRouteController.listOptimizedRoute);
 router.get("/:id", optimizedRouteController.getOptimizedRoute);
 router.patch("/edit/:id", optimizedRouteController.editOptimizedRoute);
